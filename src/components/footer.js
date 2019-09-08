69f52191ac63d1ce330b6bbf1a9adaaf9e8c297f
Kuldyaev VV <slavalion1@gmail.com>
2019-09-08 00:20:57 -0400

    2nd footer добавил иконки соцсетей

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import Img from 'react-image'
+
 
 
 class Footer extends Component{
     render(){
+        const Logo1 = () => <Img src='./img/icons/odnoklassniki.svg' />
+        const Logo2 = () => <Img src='./img/icons/facebook.svg' />
+        const Logo3 = () => <Img src='./img/icons/social.svg' />
+        const Logo4 = () => <Img src='./img/icons/linkedin.svg' />        
+        const Logo5 = () => <Img src='./img/icons/twitter(1).svg'/>
+        const Logo6 = () => <Img src='./img/icons/instagram.svg' />         
         return(
-        <div className='main'>
-            <p>Footer here</p>
+        <div className='footer'>
+            <div id="social">
+                <Logo1/>
+                <Logo2/>
+                <Logo3/>
+                <Logo4/>
+                <Logo5/>
+                <Logo6/>
+            </div>
+
+
+            <div id="rights">
+                <h6>Все права защищены &copy;
+                <a href="http://slavalion.ru" title="SlavalionStudio">SlavalionStudio</a>  2019</h6>
+            </div>
         </div>
        
         )
